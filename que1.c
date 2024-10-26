f4ea5a159754cd482bc871620bf0b517046861a5
unknown <palakgujarati2106@gmail.com>
2024-10-26 13:48:35 +0530

    update

---
+++ que1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int i,j;
+    for(i=41;j<=45;i++)
+    {
+        for(j=41;j<=i;j++)
+        {
+            printf("%d ",j);
+        }
+        printf("\n");
+    }   
+    
+}     
